57a4fb3a20dd6bc1f2ec45a540ca57d793609a56
Michael Gill <michael.78912.8@gmail.com>
2018-12-11 20:36:36 -0800

    added a couple pages, top selection bar, favicon

--- main.js
+++ main.js
@@ -1,21 +1,10 @@
-// all this file does is to choose a random icon to 
-// display as favicon.ico.
 
-function setIcon() {
-    var icon = Math.floor(Math.random()) * 2;
-    var iconName;
-    if (icon === 0) {
-        iconName = 'next.png';
-    } else {
-        iconName = 'cursor.png';
-    }
-    (function() {
-        var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
-        link.type = 'image/x-icon';
-        link.rel = 'shortcut icon';
-        link.href = iconName;
-        document.getElementsByTagName('head')[0].appendChild(link);
-    })();
+
+function main () {
+    console.log(`
+    Howdy! Why are you looking at the debug console of this website?
+    if you are that suspicious, then maybe you should look at the source code.
+    https://github.com/Michael78912/Michael78912.github.io
+    Have a good day!`)
 }
-
-setIcon()
+main()
